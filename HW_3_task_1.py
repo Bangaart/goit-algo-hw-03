8c39de90ac26035c990db6f5abde5aa9371cdf0e
Arthur <planck2909@gmail.com>
2025-03-15 14:18:55 +0200

    Initial commit

---
+++ HW_3_task_1.py
@@ -0,0 +1,14 @@
+# Створіть функцію get_days_from_today(date), яка розраховує кількість днів між заданою датою і поточною датою.
+
+
+import datetime as dt
+
+def get_days_from_today(date):
+        try:
+            users_date = dt.datetime.strptime(date, "%Y-%m-%d")
+        except ValueError:
+            return f"Verify correctness of your date"
+        else:
+            return f"Elapsed days: {(dt.datetime.today() - users_date).days}"
+
+print(get_days_from_today("2005-02-06"))
